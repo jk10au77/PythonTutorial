e947c7eef9666a299af66b58d78bdec887c4d93e
Jaya Kumar Tatiparthi <tatiparthi.jaya@outlook.com>
2023-09-30 18:55:24 +0530

    Added bubble sort.

---
+++ Lesson12.py
@@ -0,0 +1,26 @@
+"""
+    The bubble sort:
+    -----------------
+"""
+
+import random       # random generator module 
+
+user_count = int(input("Enter the total number of numbers in a list: "))
+my_list = []
+swapped = True
+for i in range(user_count):
+    number = random.randint(100, 1000)
+    my_list.append(number)
+
+print(my_list)
+print('********************************************************************************************************')
+# to sort the my_list by implementing the bubble sort
+while swapped:
+    swapped = False
+    for i in range(len(my_list) - 1):                                   # we need n - 1 comparsions
+        if my_list[i] > my_list[i + 1]:                                 # compare adjacent elements
+            swapped = True                                              # a swap occured
+            my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]     # # compare adjacent elements
+            print(my_list)
+print(my_list)
+print('*******************************************************************************************************')
