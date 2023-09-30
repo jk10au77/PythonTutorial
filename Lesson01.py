47f192cb297c437b8d72d751fb6f675402181f50
Jaya Kumar Tatiparthi <tatiparthi.jaya@outlook.com>
2023-09-30 19:13:35 +0530

    renamed hello.py file to Lesson01.py

---
+++ Lesson01.py
@@ -0,0 +1,10 @@
+"""
+    This is the firts program you learn in the world of computer programming.
+    This prints the 'Hello, World' message.
+"""
+
+print("Hello, World!")  # prints the message in the double quotes.
+
+name = 'Jaya Kumar'
+hello = 'Hello,'   
+print(hello, name)
