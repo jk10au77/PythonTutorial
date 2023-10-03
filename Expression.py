9a14472340f085b616a5fb90fd82dd1ddf2f3704
Jaya Kumar Tatiparthi <tatiparthi.jaya@outlook.com>
2023-10-03 17:15:04 +0530

    added some logic.

--- Expression.py
+++ Expression.py
@@ -6,7 +6,7 @@
 import random
 operations = ['+', '-']
 
-max = 100
+
 
 def evaluate(number_1, number_2, operate):
     if operate == '+':
@@ -20,12 +20,16 @@ def evaluate(number_1, number_2, operate):
             number_1, number_2 = number_2, number_1
             value = number_1.__sub__(number_2)
             return value
-     
 
-for count in range(100):
+max = 100      
+
+for count in range(1, 101):
     number_1 = random.randint(100, 1000)
     number_2 = random.randint(100, 1000)
     operate = random.choice(operations)
-    expr = f'{number_1} {operate} {number_2}'
+    
     value = evaluate(number_1, number_2, operate)
-    print(expr, '=', value)
+    expr = f'{count}) {number_1} {operate} {number_2} = {value}'
+    print(expr)
+
+
