341e838b9f2b70f3f777a5522b04e229d40f6636
Jaya Kumar Tatiparthi <tatiparthi.jaya@outlook.com>
2023-10-09 15:33:31 +0530

    Added about pip

---
+++ package/pythonPackaging_Ecosystem.py
@@ -0,0 +1,13 @@
+"""
+                Python Packaging Ecosystem
+                --------------------------
+    There are two basic entities in the Python ecosystem to keep the world in motion.
+        a.  a central repository (in short called repo) - all available python software packages are housed in.
+            The repo of Python is called PyPI (It's short for Python Package Index) it's maintained by a workgroup 
+            named the Packaging Working Group
+        b.  a tool to access the central repository - The tool is pip (pip installs packages)
+
+
+
+
+"""
