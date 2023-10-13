f08f4b838e156caf89dd2f0607a0d42bc9ce8dd4
Jaya Kumar Tatiparthi <tatiparthi.jaya@outlook.com>
2023-10-13 13:18:26 +0530

    Added few description about class variables.

--- Lesson_31.py
+++ Lesson_31.py
@@ -15,3 +15,23 @@ example_object = ExampleClass(2)
 
 print("Contenets of __dict__ variable of class: ", ExampleClass.__dict__)
 print("Contenets of __dict__ variable of object: ", example_object.__dict__)
+
+"""
+    checking the existence of attribute.
+    -----------------------------------------
+    In contrast to other programming languages, you may not expect that all objects of the same class have the same sets 
+    of properties. See the below code
+
+"""
+class ExampleClass:
+    def __init__(self, val):
+        if val % 2 != 0:
+            self.a = 1
+        else:
+            self.b = 1
+
+
+example_object = ExampleClass(1)
+
+print(example_object.a)
+print(example_object.b)
