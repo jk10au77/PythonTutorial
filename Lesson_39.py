bc3aed4157703c39db46f53f75307b1160d874d6
jk10au77 <tatiparthi.jaya@outlook.com>
2023-10-16 18:30:37 +0530

    Added Polymorphisim in Python.

---
+++ Lesson_39.py
@@ -0,0 +1,23 @@
+"""
+    How to build hierarchy of classes:
+    ----------------------------------
+        a.  polymorphism helps the developer to keep the code clean and consistent.
+
+
+"""
+import time
+
+class Vehicle:
+    
+    def change_direction(Left, on):
+        pass
+
+    def turn()
+
+
+
+class TrackedVehicle(Vehicle):
+    pass
+
+class WheeledVehicle(Vehicle):
+    pass
