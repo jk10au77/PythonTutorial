879fb78b9e100735f767e21ad17f0bf0b039fba3
jk10au77 <tatiparthi.jaya@outlook.com>
2023-10-19 19:52:02 +0530

    Added write() method description

---
+++ Lesson_49.py
@@ -0,0 +1,9 @@
+"""
+    Writng content to text file
+    ---------------------------
+    a.  write("string_to_be_transferred") - writes the string_to_be_transferred to an open file.
+    b.  No newline character is added to the write()'s argument. So we have to add 
+    Note:   Writing a file opened in the read mode raises an error
+
+
+"""
