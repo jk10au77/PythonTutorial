435e23451486fbebb6a5163b42cb29afd98acfd5
jk10au77 <tatiparthi.jaya@outlook.com>
2023-10-21 08:56:33 +0530

    Added about the writing contents to a new file.

--- Lesson_49.py
+++ Lesson_49.py
@@ -7,3 +7,29 @@
 
 
 """
+
+from os import strerror
+
+try:
+    fo = open("C:\\Users\\tatip\\OneDrive\\Desktop\\write.txt", 'wt')   # a new file (write.txt) is created.
+    for i in range(10):
+        s = "line #" + str(i + 0) + "\n"
+        for ch in s:
+            fo.write(ch)
+    fo.close()
+except IOError as e:
+    print("IOError occured: ", strerror(e.errno))
+
+# we have opened the above created write file to view its contents.
+try:
+    cnt = 0
+    s = open('C:\\Users\\tatip\\OneDrive\\Desktop\\write.txt', 'rt')
+    content = s.read()
+    for ch in content:
+        print(ch, end='')
+        cnt += 1
+    s.close()
+    print("\n\nTotal characters in file: ", cnt)
+except IOError as e:
+    
+    print("I/O error occured: ", strerror(e.errno))
