dacb032cc573295c8c17ac807bd84f3ae452d76c
jk10au77 <tatiparthi.jaya@outlook.com>
2023-10-25 17:59:21 +0530

    print

--- Lesson01.py
+++ Lesson01.py
@@ -8,3 +8,13 @@ print("Hello, World!")  # prints the message in the double quotes.
 name = 'Jaya Kumar'
 hello = 'Hello,'   
 print(hello, name)
+
+def printcalendar(year):
+    for yr in year:
+        for month in ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+                  'October', 'November', 'December']:
+                print(f'\t {month} {yr}')
+                print("Mo  ","Tu  ","We  ","Th  ","Fr  ","Sa  ","Su")
+                print('\n\n')
+            
+printcalendar([2020, 2021, 2022, 2023])
