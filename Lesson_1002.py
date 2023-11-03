1b93c67aca3ed49db26935631f73f13a56a4c22e
jk10au77 <tatiparthi.jaya@outlook.com>
2023-11-03 11:10:43 +0530

    Added topics to be dealt in Python advanced tutorials.

---
+++ Lesson_1002.py
@@ -0,0 +1,50 @@
+"""
+            Advanced Perspective of Classes and OOP
+            ---------------------------------------
+    In this class, you will learn about the following topics:
+
+        a.  Classes, instances, attributes, methods, as well as working with class and instance data;
+        b.  Shallow and deep operations;
+        c.  Abstract classes, method overriding, static and class methods, special methods;
+        d.  Inheritance, polymorphism, subclasses, and encapsulation;
+        e.  Advanced exception handling techniques;
+        f.  The pickle and shelve modules;
+        G.  metaclasses.
+
+            Classes, Instances, Attributes, Methods — introduction
+            ------------------------------------------------------
+    
+    Definitions:
+    -----------
+        a.  class — an idea, blueprint, or recipe for an instance;
+        b.  instance — an instantiation of the class; very often used interchangeably with the term 'object';
+        c.  object — Python's representation of data and methods; objects could be aggregates of instances;
+        d.  attribute — any object or class trait; could be a variable or method;
+        e.  method — a function built into a class that is executed on behalf of the class or object; 
+            some say that it's a 'callable attribute';
+        f.  type — refers to the class that was used to instantiate the object.
+Note:
+----
+    a.  Everything in python is an object (functions, modules, lists, etc.)
+    b.  Even classes are instances. 
+
+Why is everything in Python organized as objects?
+    Because an object is a very useful culmination of all the terms described above:
+
+        a.  it is an independent instance of class, and it contains and aggregates some specific and valuable 
+            data in attributes relevant to individual objects;
+        b.  it owns and shares methods used to perform actions.
+
+The following topics will be dealt in this advanced python tutorial
+-------------------------------------------------------------------
+a.  creation and use of decorators
+b.  implementation of core syntax;
+c.  class and static methods;
+d.  abstract methods;
+e.  comparison of inheritance and composition;
+f.  attribute encapsulation;
+g.  exception chaining;
+h.  object persistence;
+i.  metaprogramming.
+
+"""
