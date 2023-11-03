5cd2702e94237dfc793c8366c357386addb4c88a
jk10au77 <tatiparthi.jaya@outlook.com>
2023-11-03 10:37:19 +0530

    implemented few date time methods

--- Lesson_52.py
+++ Lesson_52.py
@@ -3,6 +3,35 @@ class time:
         self.hour = hour
         self.minute = minute
         self.second = second
-    
+
+    def __eq__(self, value):
+        return self.hour * 3600 + self.minute * 60 + self.second == value.hour * 3600 + value.minute * 60 + value.second
     
 
+    def __ge__(self, value):
+        return self.hour * 3600 + self.minute * 60 + self.second >= value.hour * 3600 + value.minute * 60 + value.second
+    
+    def __gt__(self, value):
+        return self.hour * 3600 + self.minute * 60 + self.second > value.hour * 3600 + value.minute * 60 + value.second 
+    
+    def __le__(self, value):
+        return self.hour * 3600 + self.minute * 60 + self.second <= value.hour * 3600 + value.minute * 60 + value.second
+    
+    def __lt__(self, value):
+        return self.hour * 3600 + self.minute * 60 + self.second < value.hour * 3600 + value.minute * 60 + value.second
+    
+    def __ne__(self, value):
+        return self.hour * 3600 + self.minute * 60 + self.second != value.hour * 3600 + value.minute * 60 + value.second
+    
+    def isoformat(self):
+        return f'{self.hour}:{self.minute}:{self.second}'
+    
+
+
+time_1  = time(10, 23, 24)
+time_2  = time(11,12,9)
+print(time_1 == time_2)
+print(time_1 > time_2)
+print(time_1 < time_2)
+print(time_2 != time_1)
+print(time_1.isoformat())
