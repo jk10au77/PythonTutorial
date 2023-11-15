40b6de37500b8db5811eb07dbc4af472197951a7
jk10au77 <tatiparthi.jaya@outlook.com>
2023-11-15 12:38:51 +0530

    Regular Expression

---
+++ Lesson_502.py
@@ -0,0 +1,30 @@
+"""
+    Regular expression:
+    -------------------
+    a.  Regular expression are special collections of characters used to describe or search for patterns in a 
+        given string.
+    b.  Regular expressions are used for data cleaning or pattern matching in text files.
+
+    What is Python Regular expression (RegEx)?
+
+        a.  A python regular expression is a sequence of metacharacters that define a search pattern.
+        b.  We use these patterns in a string-searching algorithm to "find" or "find and replace" on strings.
+        c. The term "Regular Expression" is frequently shortend to "RegEx".
+
+    Python RegEx: Metacharacters
+    ----------------------------
+    a.  Every character in a Python RegEx is either a metacharacter or a regular character.
+    b.  A metacharater has a special character, wheras a regular character matches itself.
+
+    Metcharacters:
+    --------------
+    https://www.simplilearn.com/tutorials/python-tutorial/python-regular-expressions
+    
+    1.  The '^' : checks if the string starts with a particular character or a particular word.
+    2.  The '$' : checks if the string ends with a particular character or a particular word.
+    3.  The '|' : checks the either / or condition.
+    4.  The '+' : checks one or more occurence of a character in the string.
+    5.  The '*' : checks zero or more occurence of a character in a string.
+
+"""
+import re   # import regular expression module
