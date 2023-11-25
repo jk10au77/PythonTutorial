71ae23fa5727a00dc16b8115c742d97f23f02067
jk10au77 <tatiparthi.jaya@outlook.com>
2023-11-25 09:22:59 +0530

    Added class decorator decorating the function.

--- function_decorator.py
+++ function_decorator.py
@@ -99,7 +99,7 @@ a.  the outer_decorator is called to call the inner_decorator, then the inner_de
 b.  when your function ends it execution, the inner_decorator takes over control, and after it finishes its execution, the outer_decorator is 
     able to finish its job.
 """
-
+"""
 def big_container(collective_material):
     def wrapper(our_function):
         def internal_wrapper(*args):
@@ -138,3 +138,103 @@ pack_books("Alice in Wonderland", "Winne in Pooh")
 pack_toys("doll", "car")
 pack_fruits("plum", "pear")
 
+"""
+
+"""
+Decorating functions with class:
+--------------------------------
+a.  In Python, decorators can be either function or classes. In both cases, decorating adds functionality to existing function.
+b.  When we decorate a function with a class, the function becomes an instance of the class. When we define methods in the decorating class,
+    we can add functionality to the function. This can be achieved without modifying the original function source code.
+c.  Here we demostrate two cases:
+    1.  Decorating a function with a class that accepts no arguments.
+    2.  Decorating a function with a class that accepts arguments.
+
+"""
+
+
+
+"""
+    a.  To decorate a function with a class, we must use the @ syntax followed by the class name above the function definiton. 
+    b.  When we decorate a function with a class, the function is automatically passed as a first arguments to the init constructor. We set this
+        function as an attribute in our object. Now if we print multiply_together, we can see it as an instance of the power class.
+    c.  In order to define a class as a decorator, we have to use a __call__ special class method.
+    d.  When a user needs to create an object that acts as a function (i.e., it is callable) then the function decorator needs to return an 
+        object that is callable, so the __call__ special method will be very useful.
+
+    Class definition:
+    ------------------
+
+    a.  class name: SimpleDecorator
+    b.  class methods:
+        1.  __init__(self)
+        2.  __call__(self)
+    
+    Function definition:
+    --------------------
+a.  function name:  combiner()  with variable positional and keyword arguments
+  
+"""
+
+class SimpleDecorator:
+    """
+    A short explaination of special methods:
+
+        a.  the __init__ method assigns a decorated function reference to the self attribute for later use.
+        b.  the __call__ method is responsible for supporting a case when an object is called. calls a previously referenced function.
+
+    Note:   
+    ------
+    a.  The advantage of this approach, when compared to decorators expressed with functions, is:
+        classes bring all the subsidiarity they can offer, like inheritance and the ability to create dedicated supportive methods.
+
+"""
+
+    def __init__(self, own_function):
+        self.func = own_function
+
+    def __call__(self, *args, **kwargs):
+        print('"{}" was called with the following arguments'.format(self.func.__name__))
+        print('\t{}\n\t{}\n'.format(args, kwargs))
+        self.func(*args, **kwargs)
+        print('Decorator is still operating')
+
+
+@SimpleDecorator
+def combiner(*args, **kwargs):
+    print("\tHello from the decorated function; received arguments:", args, kwargs)
+
+combiner('a', 'b', exec='yes')
+
+"""
+    class decorators with arguments:
+
+"""
+
+class WarehouseDecorators:
+
+    def __init__(self, material):
+        self.material = material
+    
+    def __call__(self, own_function):
+        def internal_wrapper(*args, **kwargs):
+            print('<strong>*</strong> Wrapping with items from {} with {}'.format(own_function.__name__, self.material))
+            own_function(*args, **kwargs)
+            print()
+        return internal_wrapper
+
+@WarehouseDecorators('Kraft')
+def pack_books(*args):
+    print("We will pack books: ", args)
+
+@WarehouseDecorators('foil')
+def pack_toys(*args):
+    print("we will pack toys: ", args)
+
+@WarehouseDecorators('cardboard')
+def pack_fruits(*args):
+    print("We will pack fruits: ", args)
+
+pack_books('Alice in Wonderland', 'Winnie the Pooh')
+pack_toys('doll', 'car')
+pack_fruits('plum', 'pear')
